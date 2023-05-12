e3655b839332788053b389fabbf229227970568f
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2023-05-12 22:17:48 +0300

    finished two tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,11 +1,7 @@
-// Описаний в документації
 import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// Add imports above this line
 import { galleryItems } from './gallery-items';
 
-// Change code below this line
 const galleryList = document.querySelector('.gallery');
 const galleryMarkup = createGalleryMarkup(galleryItems);
 galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
